0bd091eeda39e5f9b6c934386d71815f3106b42e
Shubh-Tried <shubhcoding411@gmail.com>
2026-03-05 13:47:07 +0530

    First commit for navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          NO man I don't need to learn about react
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <h1>This is a test page</h1>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">TestApp</div>
+      <ul className="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/services">Services</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
